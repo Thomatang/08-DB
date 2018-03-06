ed39cc4e1eb93849deb6b0f4d64b19913c90e33d
Thomas Bouchard <thomaswardbouchard@gmail.com>
2018-03-06 10:19:02 +0100

    hiking exercises Create and read part done

--- create.php
+++ create.php
@@ -1,12 +1,68 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "popo8989";
+$dbname = "reunion_island";
+
+//  FIRST ATTEMPT BEGINNNING ===============================
+
+if(isset($_POST['submit'])){
+	echo "yoooo";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = "INSERT INTO hiking ( name, difficulty, distance, duration, height_difference)
+        VALUES (:name, :difficulty, :distance,:duration, :height_difference)";
+
+// prepare sql and bind parameters
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':name',$name);
+    $stmt->bindParam(':difficulty',$difficulty);
+	$stmt->bindParam(':distance',$distance);
+	$stmt->bindParam(':duration',$duration);
+	$stmt->bindParam(':height_difference',$height_difference);
+// use exec() because no results are returned
+
+//insert a row
+$name = $_POST['name'];
+$difficulty = $_POST['difficulty'];
+$distance = $_POST['distance'];
+$duration = $_POST['duration'];
+$height_difference = $_POST['height_difference'];
+    $stmt->execute();
+	echo "New record created successfully";
+	
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+}
+
+//  FIRST ATTEMPT END ===============================
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Ajouter une randonnée</title>
 	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
-	<a href="/php-pdo/read.php">Liste des données</a>
+	<a href="read.php">Liste des données</a>
 	<h1>Ajouter</h1>
 	<form action="" method="post">
 		<div>
@@ -37,7 +93,8 @@
 			<label for="height_difference">Dénivelé</label>
 			<input type="text" name="height_difference" value="">
 		</div>
-		<button type="submit" name="button">Envoyer</button>
+		<input type="submit" value="submit" name="submit" />
 	</form>
+
 </body>
 </html>
--- read.php
+++ read.php
@@ -1,73 +1,67 @@
 <?php
-$db_host = 'localhost'; // Server Name
-$db_user = 'root'; // Username
-$db_pass = 'popo8989'; // Password
-$db_name = 'reunion_island'; // Database Name
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-if (!$conn) {
-	die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
-}
 
-$sql = 'SELECT * 
-		FROM hiking';
-		
-$query = mysqli_query($conn, $sql);
 
-if (!$query) {
-	die ('SQL Error: ' . mysqli_error($conn));
-}
+echo " <h1>Liste des randonnées</h1>";
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>Id</th><th>Name</th><th>Difficulty</th><th>Distance</th><th>Duration</th><th>height_difference</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+
+
+$servername = "localhost";
+$username = "root";
+$password = "popo8989";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=reunion_island", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    $stmt = $conn->prepare("SELECT * FROM hiking"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+$conn = null;
+echo "</table>";
+
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Randonnées</title>
+    <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
     
-
-<h1>Hikerdom</h1>
-	<table class="data-table">
-		<caption class="title">Some Volcano-themed hiking trails to start with:</caption>
-		<thead>
-			<tr>
-				<th>NO</th>
-				<th>NAME</th>
-				<th>DIFFICULTY</th>
-				<th>DISTANCE</th>
-				<th>DURATION</th>
-                <th>HEIGHT-DIFFERENCE</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php
-        
-        while ($row = mysqli_fetch_array($query))
-		{
-		
-			echo '<tr>
-					<td>'.$row['id'].'</td>
-					<td>'.$row['name'].'</td>
-					<td>'.$row['difficulty'].'</td>
-					<td>'. $row['distance'] . '</td>
-                    <td>'.$row['duration'].'</td>
-                    <td>'.$row['height_difference'].'</td>
-                </tr>';
-        }
-		?>
-		</tbody>
-		<tfoot>
-			<tr>
-				<h2>Let's Go Hiking!</h2>
-			</tr>
-		</tfoot>
-	</table>
-
-
-</body>
+  </body>
 </html>
